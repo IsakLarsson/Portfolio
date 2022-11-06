610245114c50314b0fd3c0fcce1f34324df8de82
Isak Larsson <isaklarsson98@hotmail.com>
2022-11-06 21:46:34 +0100

    shitcode

--- portfolio3.0/pages/index.jsx
+++ portfolio3.0/pages/index.jsx
@@ -1,103 +1,107 @@
-import anime from "animejs/lib/anime.js";
-import { Center, Flex } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
-import { motion } from "framer-motion";
-import About from "../sections/About";
+import anime from 'animejs/lib/anime.js'
+import { Center, Flex } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+import { motion } from 'framer-motion'
+import About from '../sections/About'
 
 const projects = [
-  "This would be my first project info board",
-  "This is my second project hehe",
-  "as well as this one",
-];
+    'This would be my first project info board',
+    'This is my second project hehe',
+    'as well as this one',
+]
 
 export default function Home() {
-  const hasWindow = typeof window !== "undefined";
-
-  const [columns, setColumns] = useState(0);
-  const [rows, setRows] = useState(0);
-  const [toggled, setToggled] = useState(true);
-  const [count, setCount] = useState(-1);
-  function getWindowHeight() {
-    const height = hasWindow ? window.innerHeight : null;
-    return height;
-  }
-
-  let tiles = Array.from(Array(rows * columns));
-
-  const colors = ["rgb(255, 183, 3)", "rgb(251, 133, 0)"];
-  const handleClick = (event, index) => {
-    setCount((prevCount) => prevCount + 1);
-    setToggled(!toggled);
-    anime({
-      targets: "span",
-      // opacity: toggled ? 0 : 1,
-      backgroundColor: [colors[count % (colors.length - 1)]],
-      delay: anime.stagger(50, {
-        grid: [columns, rows],
-        from: tiles.length / 2,
-      }),
-    });
-  };
-
-  useEffect(() => {
-    if (hasWindow) {
-      function handleResize() {
-        setColumns(Math.floor(document.body.clientWidth / 50));
-        setRows(Math.floor(getWindowHeight() / 50));
-        document
-          .getElementById(styles.tiles)
-          .style.setProperty("--columns", columns);
-        document.getElementById(styles.tiles).style.setProperty("--rows", rows);
-      }
-      handleResize();
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
+    const hasWindow = typeof window !== 'undefined'
+    const [columns, setColumns] = useState(0)
+    const [rows, setRows] = useState(0)
+    const [toggled, setToggled] = useState(true)
+    const [count, setCount] = useState(-1)
+    function getWindowHeight() {
+        const height = hasWindow ? window.innerHeight : null
+        return height
     }
-  });
+    let tiles = Array.from(Array(rows * columns))
 
-  const helloPhrase = ["Hello", "I'm Isak "];
+    const colors = ['rgb(255, 183, 3)', 'rgb(251, 133, 0)']
+    const handleClick = (event, index) => {
+        setCount((prevCount) => prevCount + 1)
+        setToggled(!toggled)
+        anime({
+            targets: 'span',
+            // opacity: toggled ? 0 : 1,
+            backgroundColor: [colors[count % (colors.length - 1)]],
+            delay: anime.stagger(50, {
+                grid: [columns, rows],
+                from: tiles.length / 2,
+            }),
+        })
+    }
 
-  return (
-    <main>
-      <div className={styles.main}>
-        <Center className={styles.front}>
-          <Flex direction={"column"} alignItems={"center"}>
-            {helloPhrase.map((word, i) => (
-              <motion.h1
-                key={i}
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  type: "spring",
-                  damping: 20,
-                  stiffness: 200,
-                  delay: i * 2,
-                }}
-                style={{ fontSize: "5rem", margin: "0.8rem 0" }}
-              >
-                {word}
-              </motion.h1>
-            ))}
+    useEffect(() => {
+        if (hasWindow) {
+            function handleResize() {
+                setColumns(Math.floor(document.body.clientWidth / 50))
+                setRows(Math.floor(getWindowHeight() / 50))
+                document
+                    .getElementById(styles.tiles)
+                    .style.setProperty('--columns', columns)
+                document
+                    .getElementById(styles.tiles)
+                    .style.setProperty('--rows', rows)
+            }
+            handleResize()
+            window.addEventListener('resize', handleResize)
+            return () => window.removeEventListener('resize', handleResize)
+        }
+    })
 
-            <h1
-              style={{ fontSize: "3rem", color: "black", textAlign: "center" }}
-            >
-              Welcome to my portfolio!
-            </h1>
-          </Flex>
-        </Center>
-        <div id={styles.tiles}>
-          {tiles.map((tile, index) => (
-            <span
-              onClick={(event) => handleClick(event, index)}
-              className={styles.tile}
-              key={`tile${index}`}
-            ></span>
-          ))}
-        </div>
-      </div>
-      {!toggled && <About />}
-    </main>
-  );
+    const helloPhrase = ['Hello', "I'm Isak "]
+
+    return (
+        <main>
+            <div className={styles.main}>
+                <Center className={styles.front}>
+                    <Flex direction={'column'} alignItems={'center'}>
+                        {helloPhrase.map((word, i) => (
+                            <motion.h1
+                                key={i}
+                                initial={{ opacity: 0, y: 100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{
+                                    type: 'spring',
+                                    damping: 20,
+                                    stiffness: 200,
+                                    delay: i * 2,
+                                }}
+                                style={{ fontSize: '5rem', margin: '0.8rem 0' }}
+                            >
+                                {word}
+                            </motion.h1>
+                        ))}
+
+                        <h1
+                            style={{
+                                fontSize: '3rem',
+                                color: 'black',
+                                textAlign: 'center',
+                            }}
+                        >
+                            Welcome to my portfolio!
+                        </h1>
+                    </Flex>
+                </Center>
+                <div id={styles.tiles}>
+                    {tiles.map((tile, index) => (
+                        <span
+                            onClick={(event) => handleClick(event, index)}
+                            className={styles.tile}
+                            key={`tile${index}`}
+                        ></span>
+                    ))}
+                </div>
+            </div>
+            {!toggled && <About />}
+        </main>
+    )
 }
--- portfolio3.0/sections/About.jsx
+++ portfolio3.0/sections/About.jsx
@@ -1,40 +1,83 @@
-import React, { useRef, useEffect } from "react";
-import { Box, Center, Container, Flex } from "@chakra-ui/react";
-import { animate, motion, useTransform } from "framer-motion";
-import { useInView } from "framer-motion";
+import React, { useRef, useEffect } from 'react'
+import {
+    Grid,
+    Center,
+    Box,
+    Container,
+    Flex,
+    HStack,
+    VStack,
+    GridItem,
+} from '@chakra-ui/react'
+import { animate, motion, useTransform } from 'framer-motion'
+import { useInView } from 'framer-motion'
 const About = () => {
-  const aboutRef = useRef(null);
-  const isInView = useInView(aboutRef);
-  useEffect(() => {
-    console.log(isInView);
-    return () => {};
-  }, [isInView]);
+    const aboutRef = useRef(null)
+    const isInView = useInView(aboutRef)
 
-  return (
-    <section>
-      <Flex direction={"column"} alignItems={"center"} height={"100vh"}>
-        <motion.div
-          ref={aboutRef}
-          initial={{ scale: 0 }}
-          animate={{ scale: isInView ? 1 : 0 }}
-          whileHover={{ scale: 1.2 }}
-        >
-          <h1>About</h1>
-        </motion.div>
-        <Container>
-          <p>
-            A little text about me and what I do theres nothing to see here this
-            is more of a placeholder just so you have something to fill out the
-            void that is this page
-          </p>
-        </Container>
-        <motion.div
-          initial={{ borderRadius: 10 }}
-          whileHover={{ borderRadius: 20 }}
-        ></motion.div>
-      </Flex>
-    </section>
-  );
-};
+    const projects = [
+        'This would be my first project info board',
+        'This is my second project hehe',
+        'as well as this one',
+    ]
+    useEffect(() => {
+        console.log(isInView)
+        return () => {}
+    }, [isInView])
 
-export default About;
+    return (
+        <section>
+            <Flex
+                height={'100vh '}
+                width={'100%'}
+                direction={'column'}
+                alignItems={'center'}
+            >
+                <Grid
+                    templateRows='repeat(2, 1fr)'
+                    templateColumns='repeat(2, 1fr)'
+                    gap={4}
+                >
+                    <GridItem colSpan={2}>
+                        <h1>Projects</h1>
+                    </GridItem>
+                    <VStack>
+                        {projects.map((project) => (
+                            <GridItem
+                                rowSpan={1}
+                                colSpan={1}
+                                bg={'#FFF8F0'}
+                                borderRadius={5}
+                                w={250}
+                            >
+                                <p>some text</p>
+                            </GridItem>
+                        ))}
+                    </VStack>
+                    <GridItem>
+                        <VStack>
+                            <h1>Titel</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur. Nec
+                                sodales sollicitudin urna volutpat diam id
+                                viverra massa sed. Sit pellentesque porta arcu
+                                amet. Sapien ac non vel placerat nibh elit proin
+                                suspendisse aliquet. Nunc aliquet quis integer
+                                tincidunt volutpat dui ut ipsum. Auctor sed eget
+                                sed pretium nisi risus et cursus turpis. Id
+                                augue nunc elit urna. Posuere sed ac ac
+                                scelerisque et sit ipsum velit bibendum. Id
+                                cursus orci mi faucibus egestas ut turpis
+                                malesuada aenean. Mattis fermentum massa
+                                lobortis mattis odio suspendisse scelerisque.
+                                Pulvinar quam.
+                            </p>
+                        </VStack>
+                    </GridItem>
+                </Grid>
+            </Flex>
+        </section>
+    )
+}
+
+export default About
